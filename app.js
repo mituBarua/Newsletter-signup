bd6c9f5daaadc3afdc8d54ee7c47fb5c53198f73
mituBarua <mitubarua766@gmail.com>
2020-08-04 10:54:57 +0600

    newsletter-signup project initial commit

---
+++ app.js
@@ -0,0 +1,76 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("https");
+
+const app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+
+
+ app.get("/",function(req, res){
+    res.sendFile(__dirname +"/signup.html");
+});
+
+
+ app.post("/", function(req,res){
+
+ const firstName = req.body.fName;
+ const lastName =req.body.lName;
+ const email = req.body.email;
+
+ var data = {
+    members:[
+        {
+            email_address:email,
+            status:"subscribed",
+            merge_fields:{
+                FNAME:firstName,
+                LNAME: lastName
+            }
+        }
+    ]
+ };
+
+ const jsonData=JSON.stringify(data);
+ const url =" https://us4.api.mailchimp.com/3.0/lists/847c50b6ca";
+ const options ={
+    method:"POST",
+    auth: "mitu:d578aac78e212d0539bf568b22978c65-us4"
+ }
+
+ const request= https.request(url,options,function(response){
+   if( response.statusCode === 200){
+       res.sendFile(__dirname+"/success.html");
+
+   }
+   else{
+       res.sendFile(__dirname+ "/failure.html");
+   }
+   response.on("data",function(data){
+    console.log(JSON.parse(data));
+
+ })
+ })
+ request.write(jsonData);
+ request.end();
+
+
+
+ });
+ app.post("/failure",function(req,res){
+    res.redirect("/");
+ })
+
+ app.listen(process.env.PORT || 3000 ,function(){
+    console.log("Server is loading from port 3000");
+ });
+
+
+
+
+//API KEEY
+//a8413838abda97f161205ebe6ff53715-us18
+//list id
+//18b0113e80
